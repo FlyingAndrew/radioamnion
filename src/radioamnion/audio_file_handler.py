d92444b44112d7917088c3d17884a073de466438
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-12-15 10:42:47 +0100

    support for multiple file formats, like mp3

--- src/radioamnion/audio_file_handler.py
+++ src/radioamnion/audio_file_handler.py
@@ -1,4 +1,7 @@
 import os
+import warnings
+
+import librosa
 import scipy.io.wavfile
 import numpy as np
 
@@ -11,12 +14,34 @@ class AudioFileHandler:
 
         self.time = None
 
-        self.read_file()
+        try:
+            self.read_file_scipy()
+        except ValueError:  # ValueError will come up for mp3 files
+            self.read_file_librosa()
 
-    def read_file(self):
+    def read_file_scipy(self):
+        """Read the audio file with scipy.io.wavfile.read. Faster but onyl supports `.wav` files.
+        """
         # load the data
         self.sample_rate, self.data = scipy.io.wavfile.read(self.file_name)
-        self.time = np.arange(self.data.shape[0], dtype=float) / self.sample_rate
-
         self.data = self.data.T  # self.data[0] is selected_channel 0
+
+        self.__time_and_mono_ch__()
+
+    def read_file_librosa(self, ):
+        """Read the audio file with librosa.load. Slower but supports more file typs  e.g. `.mp3`.
+        """
+        # load the data
+        with warnings.catch_warnings():
+            self.data,  self.sample_rate = librosa.load(self.file_name, sr=None, mono=False)
+
+        # librosa exports the data as float in the range [-1,1[ -> *2**15 will give the int16 format.
+        self.data = (self.data * 2 * 15).astype(np.int16)  # self.data[0] is selected_channel 0
+
+        self.__time_and_mono_ch__()
+
+    def __time_and_mono_ch__(self):
+        """ Calculate the timestamps and adds the mono channel as data[2]"""
+        self.time = np.arange(self.data.shape[-1], dtype=float) / self.sample_rate
         self.data = np.append(self.data, [self.data.sum(axis=0) // 2], axis=0)
+
